31bcfa577bd1a59d292f4c1e843ea67159a72b5e
dimanranawaka <dimanranawaka@gmail.com>
2023-09-16 19:40:07 +0530

    Comments

---
+++ Spring_01/src/main/java/lk/ijse/spring/pojo/DBConnection.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component // This will help to identify the current file as a component
+public class DBConnection {
+    public DBConnection(){
+        System.out.println("DBConnection : Instantiated");
+    }
+    public void testConnection(){
+        System.out.println("Connection Received!");
+    }
+}
