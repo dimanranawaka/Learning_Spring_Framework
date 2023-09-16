779c2e82f432cc32b637d178cf1f85e43290d3ac
dimanranawaka <dimanranawaka@gmail.com>
2023-09-16 19:45:32 +0530

    Implement - Customer.java

---
+++ Spring_01/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.pojo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customer {
+    public Customer() {
+        System.out.println("Customer:Instantiated");
+    }
+    public void getCustomerName(){
+        System.out.println("Customer Name : Diman Ranawaka");
+    }
+}
