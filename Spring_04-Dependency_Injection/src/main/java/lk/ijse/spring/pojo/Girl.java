1c0b2391dd6539c62b33a2f03836487a5e638d17
dimanranawaka <dimanranawaka@gmail.com>
2023-09-17 18:38:30 +0530

    Dependency Injection : @Autowired Annotation

--- Spring_04-Dependency_Injection/src/main/java/lk/ijse/spring/pojo/Girl.java
+++ Spring_04-Dependency_Injection/src/main/java/lk/ijse/spring/pojo/Girl.java
@@ -1,13 +1,42 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Girl {
+public class Girl implements BeanNameAware, BeanFactoryAware, ApplicationContextAware, InitializingBean, DisposableBean {
     public Girl() {
         System.out.println("Girl : Instantiated");
     }
     public void chat(){
         System.out.println("Hi, Handsome!");
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Girl : Bean Factory Awareness");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Girl : Bean Name Awareness");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Girl : Destroyed!");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Girl : Initializing the bean");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Girl : Application Context Awareness");
     }
 }
