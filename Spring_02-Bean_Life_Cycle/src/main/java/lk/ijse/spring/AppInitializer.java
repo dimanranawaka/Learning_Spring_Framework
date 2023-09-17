c328a43864ef9a95a87ed2fe81f7dee38ac1f8a0
dimanranawaka <dimanranawaka@gmail.com>
2023-09-17 07:18:41 +0530

    Initiate Second Module

---
+++ Spring_02-Bean_Life_Cycle/src/main/java/lk/ijse/spring/AppInitializer.java
@@ -0,0 +1,16 @@
+package lk.ijse.spring;
+
+import lk.ijse.spring.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+
+        context.register(AppConfig.class);
+        context.refresh();
+        context.close();
+
+    }
+}
