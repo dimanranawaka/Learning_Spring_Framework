f9e06e5bc816b6ced1c304665d4147034e4f5fa6
dimanranawaka <dimanranawaka@gmail.com>
2023-09-20 21:05:46 +0530

    Content Updated

--- Spring_05-FullMode_LightMode/src/main/java/lk/ijse/spring/config/AppConfig.java
+++ Spring_05-FullMode_LightMode/src/main/java/lk/ijse/spring/config/AppConfig.java
@@ -1,5 +1,8 @@
 package lk.ijse.spring.config;
 
+import lk.ijse.spring.pojo.PojoThree;
+import lk.ijse.spring.pojo.PojoTwo;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,5 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     public AppConfig() {
         System.out.println("AppConfig : Instantiated");
+    }
+    @Bean
+    public PojoTwo pojoTwo(){
+        return new PojoTwo();
+    }
+    @Bean
+    public PojoThree pojoThree(){
+        return new PojoThree();
     }
 }
