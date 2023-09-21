c2a71f3b053f13f3dc4ea9e00a2f5b84de613b30
dimanranawaka <dimanranawaka@gmail.com>
2023-09-21 14:21:20 +0530

    @Value Annotation : String Array

--- Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -3,14 +3,20 @@ package lk.ijse.spring.pojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
+
 @Component
 public class Customer {
     /*public Customer(@Value("IJSE")String name) {
         System.out.println("Customer : Instantiated ");
     }*/
 
-    public Customer(@Value("10") int age) {
-        System.out.println("Customer : Instantiated "+age);
+    public Customer(@Value("10,20,30,40") String names []) {
+        System.out.println("Customer : Instantiated ");
+        for (String a:
+             names) {
+            System.out.println(a);
+        }
     }
 
     //What you can assign with @Value Annotation
