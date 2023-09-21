4db87c29e54dc449de5d7d776228ddf20d34f23c
dimanranawaka <dimanranawaka@gmail.com>
2023-09-21 14:15:36 +0530

    @Value Annotation : int data

--- Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -5,7 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-    public Customer(@Value("IJSE")String name) {
+    /*public Customer(@Value("IJSE")String name) {
         System.out.println("Customer : Instantiated ");
+    }*/
+
+    public Customer(@Value("10") int age) {
+        System.out.println("Customer : Instantiated "+age);
     }
+
+    //What you can assign with @Value Annotation
+        // Primitive Data Types
+        // String Data
+        // Arrays
 }
