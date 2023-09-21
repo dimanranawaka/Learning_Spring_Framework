bbb857455917bae2073cc25b3567a23b59d5833f
dimanranawaka <dimanranawaka@gmail.com>
2023-09-21 15:26:50 +0530

    @Value Annotation : null Value : Fix

--- Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Item.java
+++ Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Item.java
@@ -1,16 +1,22 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Item {
-    // This is wrong , Cause this value is assign before the Initializing Bean(Before the object create)
+public class Item implements InitializingBean {
+
     @Value("IJSE")
     private String name;
     public Item() {
         System.out.println("Item : Instantiated");
-        System.out.println(name);
+        // This is wrong , Cause this value("IJSE") is assign before the Initializing Bean (Before the object create)
+        //System.out.println(name);
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println(name);
+    }
 }
