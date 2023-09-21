08bf2e0ee70438941e1b4c8c57bcf6cd0737feb5
dimanranawaka <dimanranawaka@gmail.com>
2023-09-21 17:08:43 +0530

    Resource Bundle

--- Spring_08-Spring_Environment/src/main/java/lk/ijse/spring/pojo/DBConnection.java
+++ Spring_08-Spring_Environment/src/main/java/lk/ijse/spring/pojo/DBConnection.java
@@ -8,8 +8,15 @@ import org.springframework.stereotype.Component;
 public class DBConnection implements InitializingBean {
     @Value("${os.name}") // Property Placeholder
     private String myOSName;
+
     @Value("${user.name}")
     private String userName; // Property Placeholder
+
+    @Value("${db.user.name}")
+    private String dbUserName; // Property Placeholder
+
+    @Value("${db.user.password}")
+    private String dbUserPassword; // Property Placeholder
     public DBConnection() {
         System.out.println("DBConnection : Instantiated");
     }
@@ -18,5 +25,7 @@ public class DBConnection implements InitializingBean {
     public void afterPropertiesSet() throws Exception {
         System.out.println(myOSName);
         System.out.println(userName);
+        System.out.println(dbUserName);
+        System.out.println(dbUserPassword);
     }
 }
