a420efea1e1144e6f3d5bf3151bb85bb7af95db1
dimanranawaka <dimanranawaka@gmail.com>
2023-09-21 14:03:17 +0530

    @Value Annotation

--- Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
+++ Spring_07-Runtime_Value_Injection/src/main/java/lk/ijse/spring/pojo/Customer.java
@@ -1,16 +1,11 @@
 package lk.ijse.spring.pojo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Customer {
-
-    // Will take this and run this as the Default Constructor , And will ignore the other one
-    public Customer() {
-        System.out.println("Default Constructor ");
-    }
-
-    public Customer(String name) {
-        System.out.println("Customer : Instantiated "+ name);
+    public Customer(@Value("IJSE")String name) {
+        System.out.println("Customer : Instantiated ");
     }
 }
