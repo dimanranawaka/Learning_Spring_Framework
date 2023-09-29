4bba4f4c0ac69d4084ff4032da481d908b72ef40
dimanranawaka <dimanranawaka@gmail.com>
2023-09-29 22:11:51 +0530

    Implement : SpringBeanOne.java

--- Spring_09-Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Spring_09-Spring_Web_MVC/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-//@ComponentScan
+@ComponentScan(basePackages = "lk.ijse.spring.pojo")
 public class WebAppConfig {
+    public WebAppConfig() {
+        System.out.println("WebAppConfig : Instantiated");
+    }
 }
