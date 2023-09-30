3840113579a4b74bf5987ad9e4116d3e4fc65a02
dimanranawaka <dimanranawaka@gmail.com>
2023-09-30 18:24:46 +0530

    Handler Mapping

--- Spring_09-Spring_Web_MVC/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring_09-Spring_Web_MVC/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/customer")// Handler Mapping : Concept that binds an url to Handler method, Which is in a Controller Class
 public class CustomerController {
-
+    //Handler Methods : Mapping methods that are inside the controllers
     @GetMapping
     public String helloSpring(){
         return "Hello I'm Spring : Your Request Received";
