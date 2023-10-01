25eceebb78e2c4ed898562013fc06a94f96d4985
dimanranawaka <dimanranawaka@gmail.com>
2023-10-01 16:40:27 +0530

    Initiate 10th Module : View Resolver

--- Spring_10-Spring_View_Controller/src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ Spring_10-Spring_View_Controller/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -1,8 +1,11 @@
 package lk.ijse.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
@@ -10,5 +13,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class WebAppConfig {
     public WebAppConfig() {
         System.out.println("WebAppConfig : Instantiated");
+    }
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
     }
 }
