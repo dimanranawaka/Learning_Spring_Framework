61415274f24dcf9b52167f6b191a3acd1557a271
dimanranawaka <dimanranawaka@gmail.com>
2023-10-05 20:20:42 +0530

    Content Updated

--- Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,12 +1,27 @@
 package lk.ijse.spring.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
     public CustomerController() {
         System.out.println("CustomerController : Instantiated");
+    }
+    @GetMapping
+    public String testOne(){
+        return "Get Method Invoked";
+    }
+    @PostMapping
+    public String testTwo(){
+        return "Post Method Invoked";
+    }
+    @PutMapping
+    public String testThree(){
+        return "Put Method Invoked";
+    }
+    @DeleteMapping
+    public String testFour(){
+        return "Delete Method Invoked";
     }
 }
