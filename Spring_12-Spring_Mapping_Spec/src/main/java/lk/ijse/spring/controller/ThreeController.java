0e7de4f97720d5bfdcd7e90cfb8a7399c02557e9
dimanranawaka <dimanranawaka@gmail.com>
2023-10-06 03:55:46 +0530

    QueryString Parameter

--- Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ThreeController.java
+++ Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/ThreeController.java
@@ -2,6 +2,7 @@ package lk.ijse.spring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,12 +14,12 @@ public class ThreeController {
     /** ?id=C001&name=IJSE -> QueryString Parameter */
 
     @GetMapping(params ={"id","name"})
-    public String testOne(){
-        return "Method One Invoked";
+    public String testOne(@RequestParam String id, @RequestParam String name){
+        return "Method One Invoked "+id+" "+name;
     }
 
     @GetMapping(params ={"address","salary"})
-    public String testTwo(){
-        return "Method Two Invoked";
+    public String testTwo(@RequestParam String address, @RequestParam String salary){
+        return "Method Two Invoked "+address+" "+salary;
     }
 }
