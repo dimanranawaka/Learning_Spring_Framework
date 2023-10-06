cf5ef9b0b9c550b3cd50356a1ce17d4e35c19958
dimanranawaka <dimanranawaka@gmail.com>
2023-10-06 21:00:19 +0530

    produce

--- Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/FourController.java
+++ Spring_12-Spring_Mapping_Spec/src/main/java/lk/ijse/spring/controller/FourController.java
@@ -1,8 +1,11 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @RequestMapping("/four")
@@ -13,11 +16,22 @@ public class FourController {
             // Body(Context)
 
     // We can narrow down request using headers also
-    @GetMapping(consumes = {"application/json"})
+    /*@GetMapping(consumes = {"application/json"})
     public String testOne(){
         return "Method one Invoked";
     }
     @GetMapping
+    public String testTwo(){
+        return "Method Two Invoked";
+    }*/
+
+    // consumes = Content-Type=application/json
+    // produce = Accept=> we want this type of response
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public String testOne(){
+        return "Method one Invoked";
+    }
+    @GetMapping(produces = {MediaType.TEXT_HTML_VALUE})
     public String testTwo(){
         return "Method Two Invoked";
     }
