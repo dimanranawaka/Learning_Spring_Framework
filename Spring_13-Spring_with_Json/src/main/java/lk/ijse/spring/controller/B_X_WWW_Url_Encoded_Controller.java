f4f3e82f116e4de6df2538dc5f48019a3e6e7874
dimanranawaka <dimanranawaka@gmail.com>
2023-10-16 06:31:35 +0530

    catch data with x-www-form-url-encoded

--- Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/B_X_WWW_Url_Encoded_Controller.java
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/B_X_WWW_Url_Encoded_Controller.java
@@ -1,4 +1,20 @@
 package lk.ijse.spring.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/form")
 public class B_X_WWW_Url_Encoded_Controller {
+
+    // 02 X-WWW-Form-Url-Encoded
+    // How to access above type data from Spring
+    // @Model Attribute
+
+    // This will catch x-www-form-url-encoded data without any issue
+    @PostMapping
+    public String receiveDataWithFormData(String id, String name,String address){
+        return "X-WWW-Form-URL-Encoded-Data : "+id+" "+name+" "+address;
+    }
 }
