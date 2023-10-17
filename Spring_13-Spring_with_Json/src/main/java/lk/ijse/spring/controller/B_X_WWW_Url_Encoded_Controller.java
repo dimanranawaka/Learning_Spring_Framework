f2b4d3d12cf129640c51764941e43d2072750ead
dimanranawaka <dimanranawaka@gmail.com>
2023-10-18 04:53:33 +0530

    x-www-form-url-encoded : (Using DTO class )

--- Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/B_X_WWW_Url_Encoded_Controller.java
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/B_X_WWW_Url_Encoded_Controller.java
@@ -1,5 +1,6 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dto.CustomerDTO;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,23 @@ public class B_X_WWW_Url_Encoded_Controller {
     // @Model Attribute
 
     // This will catch x-www-form-url-encoded data without any issue
-    @PostMapping
-    public String receiveDataWithFormData(String id, String name,String address){
-        return "X-WWW-Form-URL-Encoded-Data : "+id+" "+name+" "+address;
+    @PostMapping(path = "/a")
+    public String receiveDataWithFormData1(String id, String name,String address){
+        return "X-WWW-Form-URL-Encoded-Data1 : "+id+" "+name+" "+address;
     }
+
+    @PostMapping(path = "/aa")
+    public String receiveDataWithFormData(CustomerDTO dto){
+        return "X-WWW-Form-URL-Encoded-Data : "+dto.getId()+" "+dto.getName()+" "+dto.getAddress()+" "+dto.getSalary()+
+                " "+dto.getTp();
+    }
+
+    // OUTPUT - X-WWW-Form-URL-Encoded-Data : C002 Diman Matara 5000000.0 0772868308
+
+    @PostMapping
+    public String receiveDataWithFormData2(CustomerDTO dto){
+        return "X-WWW-Form-URL-Encoded-Data : "+dto.toString();
+    }
+
+    // OUTPUT - X-WWW-Form-URL-Encoded-Data : CustomerDTO(id=C002, name=Diman, address=Matara, salary=5000000.0, tp=0772868308)
 }
