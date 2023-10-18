7a65bd25ffd9f1612a03b52391d43f799977ac81
dimanranawaka <dimanranawaka@gmail.com>
2023-10-18 15:38:33 +0530

    Implement : AppWideExceptionHandler.java

--- Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -4,12 +4,14 @@ import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// This is also a type of aspect
 @RestControllerAdvice
 public class AppWideExceptionHandler {
 
+    // This method will invoke If Exception occurred inside the Controller methods
+
     @ExceptionHandler({RuntimeException.class})
     public ResponseUtil handleAllRuntimeExceptions(RuntimeException e){
-
         return new ResponseUtil("Error",e.getMessage(),null);
 
     }
