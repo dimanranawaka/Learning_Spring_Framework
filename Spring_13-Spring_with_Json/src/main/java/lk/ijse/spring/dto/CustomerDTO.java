6fb400289c49afe63f48b9fb82b445bc7e3cc2e9
dimanranawaka <dimanranawaka@gmail.com>
2023-10-18 07:30:35 +0530

    Json(With Jackson dependency) : ArrayList

--- Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -14,5 +16,6 @@ public class CustomerDTO {
     private String address;
     private double salary;
     private String tp;
+    private ArrayList<ItemDTO> items;
 
 }
---
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/dto/ItemDTO.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemDTO {
+    private String code;
+    private String itemName;
+}
