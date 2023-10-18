2cf6fe6a67b8c3b7bc71be7f929d1b5865751cec
dimanranawaka <dimanranawaka@gmail.com>
2023-10-18 10:16:38 +0530

    Implement : ResponseUtil.java

--- Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/E_Customer_Controller.java
+++ Spring_13-Spring_with_Json/src/main/java/lk/ijse/spring/controller/E_Customer_Controller.java
@@ -1,29 +1,41 @@
 package lk.ijse.spring.controller;
 
 import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/customer")
 public class E_Customer_Controller {
     @PostMapping
-    public void addCustomer(@RequestBody CustomerDTO dto){
-
+    public ResponseUtil addCustomer(@RequestBody CustomerDTO dto){
+        return new ResponseUtil("Ok","Successfully Added",dto);
     }
     @DeleteMapping(params = "id")
-    public void deleteCustomer(@RequestParam String id){
-
+    public ResponseUtil deleteCustomer(@RequestParam String id){
+        return new ResponseUtil("Ok","Successfully Deleted",id);
     }
     @GetMapping(params = "id")
-    public void searchCustomer(@RequestParam String id){
-
+    public ResponseUtil searchCustomer(@RequestParam String id){
+        CustomerDTO customer = new CustomerDTO("C001", "Diman", "Matara", 10000.00, "0772868308", null);
+        return new ResponseUtil("Ok","Successfully Searched",customer+" "+id);
     }
     @PutMapping
-    public void updateCustomer(@RequestBody CustomerDTO dto){
-
+    public ResponseUtil updateCustomer(@RequestBody CustomerDTO dto){
+        return new ResponseUtil("Ok","Successfully Updated",dto);
     }
     @GetMapping
-    public void getAllCustomers(){
+    public ResponseUtil getAllCustomers(){
 
+        ArrayList<CustomerDTO> allCustomers = new ArrayList<>();
+
+        allCustomers.add(new CustomerDTO("C001","Diman","Galle",10000.00,"0772868308",null));
+        allCustomers.add(new CustomerDTO("C002","Dasun","Matara",50000.00,"123",null));
+        allCustomers.add(new CustomerDTO("C003","Ranawaka","Akuressa",60000.00,"1456",null));
+        allCustomers.add(new CustomerDTO("C004","Dreed","Galle",70000.00,"125",null));
+
+        return new ResponseUtil("Ok","Successfully Loaded",allCustomers);
     }
 }
