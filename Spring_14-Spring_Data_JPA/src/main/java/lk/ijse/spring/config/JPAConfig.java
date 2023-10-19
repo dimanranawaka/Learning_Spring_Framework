348088ac62998f9c2eb547bad5b4624c45f218f1
dimanranawaka <dimanranawaka@gmail.com>
2023-10-19 17:27:30 +0530

    Implement : CustomerController.java

--- Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -4,6 +4,7 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
@@ -17,7 +18,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
-//@EnableJpaRepositories(basePackages = "lk.ijse.spring.dao")
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.dao")
 @EnableTransactionManagement
 public class JPAConfig {
 
--- Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
+++ Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -1,9 +1,21 @@
 package lk.ijse.spring.controller;
 
+import lk.ijse.spring.dao.CustomerDAO;
+import lk.ijse.spring.dto.CustomerDTO;
+import lk.ijse.spring.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @Autowired
+    CustomerDAO customerDAO;
+    @PostMapping
+    public void addCustomer(Customer customer){
+        customerDAO.save(customer);
+    }
 }
