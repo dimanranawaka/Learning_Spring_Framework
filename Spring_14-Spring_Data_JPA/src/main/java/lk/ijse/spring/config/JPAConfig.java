07d2ef5028ea23938316ddc9858781c1166e0bea
dimanranawaka <dimanranawaka@gmail.com>
2023-10-19 15:20:39 +0530

    Implement : JPAConfig.java

--- Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -3,17 +3,17 @@ package lk.ijse.spring.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-<<<<<<< HEAD
 
-=======
 import java.sql.DriverManager;
->>>>>>> origin/master
 
 
 @Configuration
@@ -47,5 +47,9 @@ public class JPAConfig {
         vad.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
         vad.setShowSql(true);
         return vad;
+    }
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory factory){
+        return new JpaTransactionManager();
     }
 }
