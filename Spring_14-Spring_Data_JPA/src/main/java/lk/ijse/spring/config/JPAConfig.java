95cbd77290b375eb790df1a24488ee59d899fb42
dimanranawaka <dimanranawaka@gmail.com>
2023-10-21 15:10:47 +0530

    Linking properties.properties

--- Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
+++ Spring_14-Spring_Data_JPA/src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -1,9 +1,12 @@
 package lk.ijse.spring.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -20,7 +23,11 @@ import javax.sql.DataSource;
 @Configuration
 @EnableJpaRepositories(basePackages = "lk.ijse.spring.repo") // should link where we are going to write our queries
 @EnableTransactionManagement // AOP -> use the transaction manager to manage transaction
+@PropertySource("classpath:properties.properties") // Links the property file
 public class JPAConfig {
+
+    @Autowired
+    Environment environment;
 
     //data base access (username,password,name,url)
     //Spring data jpa need a vendor to run ORM
@@ -30,17 +37,17 @@ public class JPAConfig {
         LocalContainerEntityManagerFactoryBean factory= new LocalContainerEntityManagerFactoryBean();
         factory.setDataSource(ds); // for access a data source
         factory.setJpaVendorAdapter(vad); // for accessing a vendor (hibernate)
-        factory.setPackagesToScan("lk.ijse.spring.entity"); // Set Entity records to Location to the Spring Data JPA
+        factory.setPackagesToScan(environment.getRequiredProperty("pro.entity")); // Set Entity records to Location to the Spring Data JPA
         return factory;
     }
 
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource ds= new DriverManagerDataSource();
-        ds.setUsername("root");
-        ds.setPassword("1234");
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost:3306/d2?createDatabaseIfNotExist=true");
+        ds.setUsername(environment.getRequiredProperty("pro.username"));
+        ds.setPassword(environment.getRequiredProperty("pro.password"));
+        ds.setDriverClassName(environment.getRequiredProperty("pro.driver"));
+        ds.setUrl(environment.getRequiredProperty("pro.url"));
         return ds;
     }
 
@@ -49,7 +56,7 @@ public class JPAConfig {
         HibernateJpaVendorAdapter va= new HibernateJpaVendorAdapter();
         va.setDatabase(Database.MYSQL); // What is the DB
         va.setGenerateDdl(true); // Data definition language enable
-        va.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect"); // platform version
+        va.setDatabasePlatform(environment.getRequiredProperty("pro.dial")); // platform version
         va.setShowSql(true); // if you wanted to see generated sql
         return va;
     }
